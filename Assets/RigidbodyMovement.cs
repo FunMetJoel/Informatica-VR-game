5f2de2d15f14caca97476cfced610f0095245e42
Thijshenk <120168286+Thijshenk@users.noreply.github.com>
2024-04-03 18:06:39 +0200

    RigidbodyMovement(Playermovement)
    Make the player be able to play

---
+++ Assets/RigidbodyMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Vector3 PlayerMovementInput;
+    private Vector2 PlayerMouseInput;
+    private float xRot;
+
+    [SerializeField] private LayerMask FloorMask;
+    [SerializeField] private Transform FeetTransForm;
+    [SerializeField] private Transform PlayerCamera;
+    [SerializeField] private Rigidbody PlayerBody;
+    [Space]
+    [SerializeField] private float speed;
+    [SerializeField] private float Sensitivty;
+    [SerializeField] private float Jumpforce;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovementInput= new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        PlayerMouseInput= new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        MovePlayer();
+        MovePlayerCamera();
+    }
+
+    private void MovePlayer ()
+    {
+        Vector3 MoveVector = transform.TransformDirection(PlayerMovementInput) * speed;
+        PlayerBody.velocity = new Vector3(MoveVector.x, PlayerBody.velocity.y, MoveVector.z);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {   
+            if(Physics.CheckSphere(FeetTransForm.position , 0.1f, FloorMask))
+            {
+                PlayerBody.AddForce(Vector3.up * Jumpforce, ForceMode.Impulse);
+            }
+        }
+    }
+
+    private void MovePlayerCamera ()
+    {
+        xRot -= PlayerMouseInput.y * Sensitivty;
+        transform.Rotate(0f, PlayerMouseInput.x * Sensitivty, 0f);
+        PlayerCamera.transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+         
+    }
+}
