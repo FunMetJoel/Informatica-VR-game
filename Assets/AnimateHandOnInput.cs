abe8cbe6f4f02f76a6da1ace36d914c3cf3dcc53
FunMetJoel <101552207+FunMetJoel@users.noreply.github.com>
2024-04-06 21:35:23 +0200

    vastpakken

--- Assets/AnimateHandOnInput.cs
+++ Assets/AnimateHandOnInput.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AnimateHandOnInput : MonoBehaviour
 {
+    public InputActionProperty pinchAnimationAction;
+    public InputActionProperty gripAnimationAction;
+
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,10 @@ public class AnimateHandOnInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float triggerValue = pinchAnimationAction.action.ReadValue<float>();
+        animator.SetFloat("Trigger", triggerValue);
+
+        float gripValue = gripAnimationAction.action.ReadValue<float>();
+        animator.SetFloat("Grip", gripValue);
     }
 }
