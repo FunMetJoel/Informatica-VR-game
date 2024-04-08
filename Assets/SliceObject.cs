9234df03e23b7d96372ab72dd9dc02f977e88bdf
Thijshenk <120168286+Thijshenk@users.noreply.github.com>
2024-04-08 23:56:27 +0200

    Almost working slice script

---
+++ Assets/SliceObject.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EzySlice;
+using UnityEngine.InputSystem;
+using TMPro;
+using JetBrains.Annotations;
+
+public class SliceObject : MonoBehaviour
+{
+    public CollisionDetection cd;
+    public WeaponController wc;
+
+    public Transform startSlicePoint;
+    public Transform endSlicePoint;
+    public LayerMask sliceableLayer;
+    public VelocityEstimator velocityEstimator;
+
+    public Material cutMaterial;
+
+    public GameObject target;
+
+    public float cutForce = 2000;
+
+    public bool hasHit = false;
+    public bool targetEmpty = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(target == null){
+            targetEmpty = true;
+        }
+        else{
+            targetEmpty = false;
+        }
+        hasHit = Physics.Raycast(startSlicePoint.position, (endSlicePoint.position - startSlicePoint.position).normalized, out RaycastHit hit) && wc.IsAttacking &&  ( hit.collider.CompareTag("Cut") || hit.collider.CompareTag("Enemy"));
+        if(hasHit && targetEmpty)
+        {
+            Debug.Log("Target set");
+            target = hit.transform.gameObject;
+        }
+
+        
+        if(hasHit && hit.collider.CompareTag("Cut"))
+        {
+            target = hit.transform.gameObject;
+            slice(target);
+        }
+    }
+
+    public void slice(GameObject target)
+    {
+        Vector3 velocity = velocityEstimator.GetVelocityEstimate();
+        Vector3 planeNormal = Vector3.Cross(endSlicePoint.position - startSlicePoint.position, velocity);
+        planeNormal.Normalize();
+
+        SlicedHull hull = target.Slice(endSlicePoint.position, planeNormal);
+
+        if(hull !=null)
+        {
+            GameObject upperHull = hull.CreateUpperHull(target, cutMaterial);
+            setupSlicedComponent(upperHull);
+            GameObject lowerHull = hull.CreateLowerHull(target, cutMaterial);
+            setupSlicedComponent(lowerHull);
+
+            Destroy(target);
+
+        }
+    }
+
+    public void setupSlicedComponent(GameObject slicedObject)
+    {
+        Rigidbody rb = slicedObject.AddComponent<Rigidbody>();
+        MeshCollider collider = slicedObject.AddComponent<MeshCollider>();
+        slicedObject.tag = "Cut";
+        collider.convex = true;
+        rb.AddExplosionForce(cutForce, slicedObject.transform.position, 1);
+    }
+    
+}
--- Assets/Wapen/CollisionDetection.cs
+++ Assets/Wapen/CollisionDetection.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class CollisionDetection : MonoBehaviour
 {
     public WeaponController wc;
+    public SliceObject so;
     private float lastDamageTime = 0f;
     public float damageCooldown = 0.55f;
+    public int weaponDamage = 1;
     private List<GameObject> hitEnemies = new List<GameObject>();    
     
     //public GameObject HitParticle;
 
 
-        private void Update()
+    private void Update()
     {
         // Check if the cooldown period has expired, then clear the list of hit enemies
         if (Time.time - lastDamageTime >= damageCooldown)
@@ -24,39 +26,58 @@ public class CollisionDetection : MonoBehaviour
 
 
 
-    private void DealDamage(GameObject enemy, bool vr)
+    public IEnumerator DealDamage(GameObject enemy, bool vr)
     {
         if(enemy == null)
         {
-            return;
+            yield break;
         }
         if((wc == null || !wc.IsAttacking) && (!vr))
         {
-            return;
+            yield break;
         }
         if(enemy.tag == "Enemy")
         {
-            if (Time.time - lastDamageTime >= damageCooldown || !hitEnemies.Contains(enemy))
+        if ((Time.time - lastDamageTime >= damageCooldown || !hitEnemies.Contains(enemy)) && wc.IsAttacking)
             {
-            Debug.Log(enemy.name);
-            enemy.GetComponent<Animator>().SetTrigger("Hit");
-            enemy.GetComponent<Health>().Damage(1);
-            lastDamageTime = Time.time;
-            hitEnemies.Add(enemy);
-            //Instantiate(HitParticle, new Vector3(other.transform.position.x, transform.position.y, other.transform.position.z), other.transform.rotation);
+                yield return new WaitUntil(() => !so.targetEmpty);                
+                // yield return new WaitForFixedUpdate();                
+                int hp = enemy.GetComponent<Health>().PublicHp;
+                Debug.Log(weaponDamage);
+                Debug.Log(hp);
+                Debug.Log(so.target);
+                Debug.Log(so.hasHit);
+                Debug.Log(so);
+                if(so != null && so.target != null){
+                if(so.hasHit && hp <= weaponDamage){
+                    Debug.Log("slice!");
+                    so.slice(so.target);
+                 }
+                 }
+                 
+                Debug.Log(enemy.name);
+                enemy.GetComponent<Animator>().SetTrigger("Hit");
+                Debug.Log(enemy.GetComponent<Health>().PublicHp);
+                enemy.GetComponent<Health>().Damage(weaponDamage);
+                lastDamageTime = Time.time;
+                hitEnemies.Add(enemy);
+                so.target = null;
+                //Instantiate(HitParticle, new Vector3(other.transform.position.x, transform.position.y, other.transform.position.z), other.transform.rotation);
             }
         }
     }
 
+    
+
     private void OnTriggerExit(Collider other) {
-        DealDamage(other.gameObject, false);   
+        StartCoroutine(DealDamage(other.gameObject, false));   
     }
 
     private void OnTriggerStay(Collider other) {
-        DealDamage(other.gameObject, false);
+        StartCoroutine(DealDamage(other.gameObject, false)); 
     }
     private void OnTriggerEnter(Collider other) {
-        DealDamage(other.gameObject, true);
+        StartCoroutine(DealDamage(other.gameObject, true)); 
     }
 }
 
