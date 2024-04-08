6f68ffc12c99b372cfafcf7aac55bc29bd68f29d
Thijshenk <120168286+Thijshenk@users.noreply.github.com>
2024-04-08 19:07:39 +0200

    Npc text based on user input
    Get the next sentence of an npc if the user is ready for it and thus presses a button

--- Assets/Npc/Npc.cs
+++ Assets/Npc/Npc.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -7,16 +8,12 @@ using UnityEngine.InputSystem;
 public class Npc : MonoBehaviour
 {
     public ShowText st;
+    public WeaponController wc;
+
     private bool InRange = false;
     private string npc;
     private bool interactionStarted = false;
-
-
-    private string npcTextList;
-    private Dictionary<string, List<string>> npcTextLists = new Dictionary<string, List<string>>();
-
-    
-    
+    private Dictionary<string, List<string>> npcTextLists = new Dictionary<string, List<string>>();    
     
     
     
@@ -24,9 +21,9 @@ public class Npc : MonoBehaviour
     void Start()
     {
         npcTextLists["Henk"] = new List<string> {"Geday Mate!", "Yodiyow gamer hoe is hett", "Dit is mijn tweede zin", "Dit is mijn derde zin." , "Vierde zin", "Vijfde zin"};
-        npcTextLists["Alice"] = new List<string> {"Text for NpcAlice"};
+        npcTextLists["Alice"] = new List<string> {"Text for Alice"};
 
-
+        wc = GetComponentInChildren<WeaponController>();
         st = GetComponentInChildren<ShowText>();    
     }
 
@@ -35,37 +32,49 @@ public class Npc : MonoBehaviour
     {
         if (InRange && Input.GetKeyDown(KeyCode.E) && !interactionStarted)
         {
-            interactionStarted = true;
-            StartCoroutine(interaction());       
+            StartCoroutine(Interaction());       
         }
     }
 
-
-    private IEnumerator interaction()
+    private IEnumerator Interaction()
     {
-        List <string> npcText = npcTextLists[npc];
+        interactionStarted = true;
+        List<string> npcText = npcTextLists[npc];
         st.header = npc;
         st.textValue = npcText[0];
+        Debug.Log("before the for");
 
-        for(int i=1; i < npcText.Count; i++)
+        for (int i = 1; i < npcText.Count; i++)
         {
             yield return new WaitForSeconds(0.1f);
-            yield return new WaitUntil(() => Input.anyKeyDown);
-            st.textValue = npcText[i];
-        }   
-        interactionStarted = false;        
-    }
 
+            yield return new WaitUntil(() => Input.anyKeyDown && !Input.GetKeyDown(KeyCode.W) && !Input.GetKeyDown(KeyCode.A) && !Input.GetKeyDown(KeyCode.S) && !Input.GetKeyDown(KeyCode.D) || !InRange);
+
+            if (!InRange)
+            {
+                interactionStarted = false;
+                yield break;
+            }
+            else
+            {
+                st.textValue = npcText[i];
+            }
+        }
+
+    interactionStarted = false;
+    }
 
     private void OnTriggerExit(Collider other) {
         InRange = false;
-        
-
+        st.header = null;
+        st.textValue = null;
+        wc.CanAttack = true;
     }
 
     private void OnTriggerEnter(Collider other) {
         if(other.tag == "Npc")
         {
+            wc.CanAttack = false;
             InRange = true;
             npc = other.gameObject.name;
             npc = npc.Remove(0,3);
