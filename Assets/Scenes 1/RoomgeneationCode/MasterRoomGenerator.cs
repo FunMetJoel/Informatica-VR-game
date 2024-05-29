1db5cfc026f045801c36465e8094a63d5f8a9fb9
Tieme-udb <101552207+FunMetJoel@users.noreply.github.com>
2024-05-29 15:28:23 +0200

    Stuff

---
+++ Assets/Scenes 1/RoomgeneationCode/MasterRoomGenerator.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterRoomGenerator : MonoBehaviour
+{
+    public List<GameObject> possiblePrefabs = new List<GameObject>();
+
+    List<Transform> RoomEndPoints = new List<Transform>();
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
