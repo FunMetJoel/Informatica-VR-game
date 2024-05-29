32bd36b2177bf9712e1fc1a3c2ea720d44d7d4a7
Tieme-udb <101552207+FunMetJoel@users.noreply.github.com>
2024-05-29 15:59:05 +0200

    Room generation start

--- Assets/Scenes 1/RoomgeneationCode/MasterRoomGenerator.cs
+++ Assets/Scenes 1/RoomgeneationCode/MasterRoomGenerator.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MasterRoomGenerator : MonoBehaviour
 {
     public List<GameObject> possiblePrefabs = new List<GameObject>();
 
-    List<Transform> RoomEndPoints = new List<Transform>();
+    [SerializeField] private List<Transform> RoomEndPoints = new List<Transform>();
 
-    
+    public bool NextIteration = false;
 
 
     // Start is called before the first frame update
@@ -20,6 +22,23 @@ public class MasterRoomGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (NextIteration)
+        {
+            NextIteration = false;
+            for (int i = 0; i < RoomEndPoints.Count; i++)
+            {
+                spawnPrefab(possiblePrefabs[0], RoomEndPoints[i]);
+                RoomEndPoints.Remove(RoomEndPoints[i]);
+            }
+        }
+    }
+
+    void spawnPrefab(GameObject prefab, Transform transform)
+    {
+        GameObject newRoom = Instantiate(prefab, transform);
+        for (int i = 0; i < newRoom.GetComponent<Room>().RoomEndPoints.Count; i++)
+        {
+            RoomEndPoints.Add(newRoom.GetComponent<Room>().RoomEndPoints[i]);
+        }
     }
 }
--- Assets/Scenes 1/RoomgeneationCode/Room.cs
+++ Assets/Scenes 1/RoomgeneationCode/Room.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
+    public List<Transform> RoomEndPoints = new List<Transform>();
     // Start is called before the first frame update
     void Start()
     {
