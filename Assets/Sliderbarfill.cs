eab7f445b97af0dfd0a3de8895efef13dac4464c
FunMetJoel <101552207+FunMetJoel@users.noreply.github.com>
2024-06-19 09:36:35 +0200

    fix?

--- Assets/Sliderbarfill.cs
+++ Assets/Sliderbarfill.cs
@@ -6,13 +6,14 @@ using UnityEngine.UI;
 public class Sliderbarfill : MonoBehaviour
 {
     public Image barFill;
+    public Transform slidecube
     //public sliderLocation slide;
     //public Loadtime Xslide;
     // Update is called once per frame
     void Update()
     {
         //the fillamount of the bar is the same as the location given in the x-way 
-        int XValue =  GetComponent<slidecube>(localposition.x);    
+        int XValue =  slidecube.localposition.x;    
         Xslide = (XValue + 45) / 90;   // hier   
         barFill.fillAmount = Xslide;     
     }
