0b62dad55664c6ccb414c765a2c54efe444f351a
Tieme-udb <101552207+FunMetJoel@users.noreply.github.com>
2024-06-20 16:09:20 +0200

    Reapply "iets"
    This reverts commit b8b880ad0d46830217b22786e0c42d502a5d65de.

--- Assets/Objects/UI/ShowPlayerHealth.cs
+++ Assets/Objects/UI/ShowPlayerHealth.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class ShowPlayerHealth : MonoBehaviour
 {
+
+    public Health healthScript; 
     [SerializeField] private int maxHealth;
     [SerializeField] private int maxShield;
     [SerializeField] private int health;
@@ -23,8 +25,9 @@ public class ShowPlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        health = healthScript.PublicHp;
         RedHeart.fillAmount = Mathf.Clamp((float)health / (float)maxHealth, 0f, 1f);
         //Debug.Log((float)health / (float)maxHealth);
-        YellowHeart.fillAmount = Mathf.Clamp(((float)health - maxHealth) / (float)maxShield, 0f, 1f);
+        YellowHeart.fillAmount = 0;//Mathf.Clamp(((float)health - maxHealth) / (float)maxShield, 0f, 1f);
     }
 }
